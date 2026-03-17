04d4e95b2af2447477852bc4a1c4a591a44ec4b9
Saran-006 <srisaranp123@gmail.com>
2026-03-17 22:17:23 +0530

    v1.02

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3500;
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
